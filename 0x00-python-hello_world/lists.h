cae7254920afddafe8db0a2f5f3a8f25a9b1c6e2
root <root@DESKTOP-JEBIJ4L.localdomain>
2022-09-19 20:24:54 +0300

    lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,24 @@
 #ifndef LISTS_H
 #define LIST_H
-int check_cycle(listint_t *list);
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
 
-#endif
+/**
+ * struct listint_s - singly linked list
+ * @n: int value stored
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+#endif 
