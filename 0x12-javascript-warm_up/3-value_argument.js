e2d87796dd24a361951616aeba3173b36c319271
priest-tech <shixkamanu@gmail.com>
2022-12-19 18:36:20 +0300

    semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const myArgs = process.argv[2];
 if (myArgs === undefined) {
-    console.log('No argument');
-    }
-    else{
-        console.log(myArgs);
-    }
+  console.log('No argument');
+} else {
+  console.log(myArgs);
+}
