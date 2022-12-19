f9deb0756d2b639e47f16ec8bf63fc8ab80c68d3
priest-tech <shixkamanu@gmail.com>
2022-12-19 20:52:10 +0300

    7-multi_c.js2

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-if (args.length < 1){
-    console.log("Missing number of occurences");
-    process.exit(1);
+if (args.length < 1) {
+  console.log('Missing number of occurences');
+  process.exit(1);
 }
 const numOccurences = parseInt(args[0]);
 
 if (isNaN(numOccurences)) {
-    console.log("Missing number of occurences");
-    process.exit(1);
+  console.log('Missing number of occurences');
+  process.exit(1);
 }
-for (let i = 0; i < numOccurences; i++){
-    console.log('C is fun');
+for (let i = 0; i < numOccurences; i++) {
+  console.log('C is fun');
 }
